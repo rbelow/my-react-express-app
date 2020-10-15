2cb9953111afbe5056bfa37f88d8dbab3b273726
Rubén Below <below@365farmnet.com>
2020-10-16 00:28:26 +0200

    feat: add routing and navigation

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
+import { Router, Route } from "react-router-dom";
+import { history } from "../store/history";
+import { ConnectedNavigation } from "./Navigation";
 
 export const Main = () => (
-  <Provider store={store}>
-    {/* <div>Dashboard Goes Here</div> */}
-    <ConnectedDashboard />
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <div>
+        <ConnectedNavigation />
+        <Route
+          exact
+          path="/dashboard"
+          render={() => <ConnectedDashboard />}
+        ></Route>
+      </div>
+    </Provider>
+  </Router>
 );
