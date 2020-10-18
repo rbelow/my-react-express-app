aad84fd4f0dc175ef3703770f63f7896fd6367fb
Rubén Below <below@365farmnet.com>
2020-10-18 15:50:56 +0200

    feat: add new task creation capabilities

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,45 @@
+import { applyMiddleware, combineReducers } from "redux";
 import { createStore } from "redux";
 import { defaultState } from "../../server/defaultState";
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
-export const store = createStore(function reducer(
-  state = defaultState,
-  action
-) {
-  return state;
-});
+const sagaMiddleware = createSagaMiddleware();
+import * as sagas from "./sagas.mock";
+import * as mutations from "./mutations";
+
+export const store = createStore(
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch (action.type) {
+        case mutations.CREATE_TASK:
+          console.log(action);
+          return [
+            ...tasks,
+            {
+              id: action.taskID,
+              name: `New Task: ${action.taskID}`,
+              group: action.groupID,
+              owner: action.ownerID,
+              isComplete: false,
+            },
+          ];
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    comments(users = defaultState.users) {
+      return users;
+    },
+  }),
+  applyMiddleware(createLogger(), sagaMiddleware)
+);
+
+for (let saga in sagas) {
+  sagaMiddleware.run(sagas[saga]);
+}
---
+++ src/app/store/sagas.mock.js
@@ -0,0 +1,17 @@
+import { take, put, select } from "redux-saga/effects";
+
+import * as mutations from "./mutations";
+import uuid from "uuid";
+
+export function* taskCreationSaga() {
+  while (true) {
+    const { groupID } = yield take(mutations.REQUEST_TASK_CREATION);
+    // const ownerID = yield select(state=>state.session.id);
+    const ownerID = `U1`;
+
+    // const taskID = uuid(); FIXME:
+    const taskID = Math.floor(Math.random() * 1000 + 1);
+    yield put(mutations.createTask(taskID, groupID, ownerID));
+    console.log("Got group ID", groupID);
+  }
+}
