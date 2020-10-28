b89620cb45213fbb04770308e3760768df0d5f65
Rubén Below <below@365farmnet.com>
2020-10-28 01:08:24 +0100

    feat: add update task route

--- src/server/server.spec.js
+++ src/server/server.spec.js
@@ -1,6 +1,13 @@
-import { addNewTask } from "./server";
+import { addNewTask, updateTask } from "./server";
 
-addNewTask({
-  name: "My task",
-  id: "12345",
-});
+(async function myFunc() {
+  await addNewTask({
+    id: "12345",
+    name: "My task",
+  });
+
+  await updateTask({
+    id: "12345",
+    name: "My task - UPDATED!",
+  });
+})();
